c4947cebdc658cc887fa6a535e62266ceb0ae5d0
alterdekim <alterwain@protonmail.com>
2024-03-13 14:25:48 +0300

    bugfix (ActionScript repository)

--- src/main/java/com/alterdekim/javabot/repository/ActionScriptsRepository.java
+++ src/main/java/com/alterdekim/javabot/repository/ActionScriptsRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ActionScriptsRepository extends JpaRepository<ActionScript, Long> {
-    Optional<ActionScript> findByScriptId(Long scriptId);
+    Optional<ActionScript> findById(Long id);
 
     List<ActionScript> findAll();
 }
