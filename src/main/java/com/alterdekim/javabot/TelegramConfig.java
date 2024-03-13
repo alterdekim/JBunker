b5f2022b0b71c5ec5777637bae316e14c663398b
alterdekim <alterwain@protonmail.com>
2024-03-13 04:20:53 +0300

    Telegram config changed

--- src/main/java/com/alterdekim/javabot/TelegramConfig.java
+++ src/main/java/com/alterdekim/javabot/TelegramConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.PropertySource;
 
 @Data
 @Configuration
-@PropertySource("file:./telegram.properties")
+@PropertySource("file:./../../spring-conf/bunker/telegram.properties")
 public class TelegramConfig { //
     @Value("${bot.botToken}") private String botToken;
     @Value("${bot.botLogin}") private String botLogin;
