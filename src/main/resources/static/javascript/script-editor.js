efed6723cab1fb72ffaaa368a60fdf9acafa4996
alterdekim <alterwain@protonmail.com>
2024-07-06 21:54:30 +0300

    script editor js added

--- src/main/resources/static/javascript/script-editor.js
+++ src/main/resources/static/javascript/script-editor.js
@@ -5,3 +5,27 @@ editor.session.setMode("ace/mode/lua");
 // set script body
 // editor.setValue(jobj.scriptBody);
 
+const snippets = ["player", "players[index]", "genders[index]", "hobbies[index]", "healths[index]", "luggages[index]", "works[index]"];
+
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null) {
+       return null;
+    }
+    return decodeURI(results[1]) || 0;
+};
+
+function pasteSnippet(index) {
+    editor.insert(snippets[index]);
+}
+
+function getActionScript() {
+    $.post("/api/edit_entry", "section="+_ss+"&entry_id="+($.urlParam("script_id")), function(data, status) {
+        var jobj = JSON.parse(data);
+        editor.setValue(jobj.scriptBody);
+    });
+}
+
+if ($.urlParam("script_id") != "-1") {
+    getActionScript();
+}
