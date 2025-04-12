9d4dd65af08a478cf2ddc44637bed3d55e2bca88
alterwain <alterwain@protonmail.com>
2025-04-12 04:40:26 +0300

    Changed action cards logic. x7

--- src/main/java/com/alterdekim/javabot/bot/cards/ActionCard.java
+++ src/main/java/com/alterdekim/javabot/bot/cards/ActionCard.java
@@ -6,11 +6,16 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 
-@AllArgsConstructor
+
 @NoArgsConstructor
 public abstract class ActionCard {
     BunkerBot bot;
     Player activator;
+
+    public ActionCard(BunkerBot bot, Player activator) {
+        this.bot = bot;
+        this.activator = activator;
+    }
 
     public abstract void execute();
 
